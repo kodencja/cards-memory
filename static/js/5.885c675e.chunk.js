1d3ebe3ed995ad4460b3a305b2dfa9706c1c25ef
kodencja <krzysztof.lalik.pl@gmail.com>
2021-06-01 13:32:03 +0200

    Updates

---
+++ static/js/5.885c675e.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmemory-cards-game"]=this["webpackJsonpmemory-cards-game"]||[]).push([[5],{65:function(s,a,e){}}]);
+//# sourceMappingURL=5.885c675e.chunk.js.map
---
+++ static/js/6.ce7d773d.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmemory-cards-game"]=this["webpackJsonpmemory-cards-game"]||[]).push([[6],{66:function(s,a,e){}}]);
+//# sourceMappingURL=6.ce7d773d.chunk.js.map
